540dd8406d8dc07a18ce98304ea37cc29d5c05a0
Zsergan <tit0497@mail.ru>
2020-01-30 16:35:47 +1000

    SPApp1

---
+++ precache-manifest.97e473c80414f4f8e80b12fb7a6ec7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f351540f42adee5fb55c99ed82ff9ce2",
+    "url": "/resumeSPA/index.html"
+  },
+  {
+    "revision": "bb5a7124281d760cfa2e",
+    "url": "/resumeSPA/static/css/main.0e1fe3ed.chunk.css"
+  },
+  {
+    "revision": "4c3746d0692f335fd179",
+    "url": "/resumeSPA/static/js/2.51a07827.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/resumeSPA/static/js/2.51a07827.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb5a7124281d760cfa2e",
+    "url": "/resumeSPA/static/js/main.4d198c52.chunk.js"
+  },
+  {
+    "revision": "1cc7f257f2bcfc95a4b9",
+    "url": "/resumeSPA/static/js/runtime-main.1eabd1e5.js"
+  }
+]);
